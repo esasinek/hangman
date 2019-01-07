a2e3dfdd85dc94ba5df65cc6887f940a84652bd4
Evi Balamoti <evibalamoti@gmail.com>
2019-01-07 20:31:55 +0100

    Hra Hangman verze 2. Pridano: - lze hrat vic her za sebou

--- hangman.py
+++ hangman.py
@@ -1,22 +1,40 @@
 from random import choice
 
-words = ["andulka", "smrk", "televize", "jablko", "kominík", "řepa", "konvalinka", "sasanka", "křišťál"]
-field = []
-unsuccessful_tries = 0
+words = ["andulka", "smrk", "televize", "jablko", "kominík", "řepa", "konvalinka", "sasanka", "křišťál", "řeřicha", "kvásek", "kobyla", "kampaň", "šalvěj", "sárí"]
 
 def choose(list_of_words):
     chosen_word = choice(words)
     return chosen_word
 
-word = choose(words)
-for letter in word:
-    field.append("_")
+
+def make_it_the_field(word):
+    field = []
+    for letter in word:
+        field.append("_")
+    return field
+
 
 def show_current(field):
     print()
     print(" ".join(field))
     print()
 
+
+def turn(field, tries):
+    show_current(field)
+    ch = (input("Napiš písmeno: ")).lower()        # character
+    list_of_letters = ["a", "á", "b", "c", "č", "d", "ď", "e", "é", "ě", "f", "g", "h", "i", "í", "j", "k", "l", "m", "n", "ň", "o", "ó", "p", "q", "r", "ř", "s", "š", "t", "ť", "u", "ú", "ů", "v", "w", "x", "y", "ý", "z", "ž"]
+    if ch in list_of_letters:
+        if ch in word:
+            indices = [i for i, x in enumerate(word) if x == ch]    # tomuhle ještě úplně nerozumím
+            for j in indices:
+                field[j] = ch
+        else:
+            print("Špatně. Toto písmeno ve slovu není. Stavíme šibenici.")
+            tries += 1
+    else:
+        print("To nebylo písmeno. Zkusíš to znovu?")
+    return field, tries
 
 def pick_picture(number_of_fails):
     if number_of_fails == 0:
@@ -121,22 +139,51 @@ def pick_picture(number_of_fails):
 ~~~~~~~
         """)
 
-
-def turn(field, tries):
-    show_current(field)
-    ch = (input("Napiš písmeno: ")).lower()        # character
-    if ch in word:
-        indices = [i for i, x in enumerate(word) if x == ch]    # tomuhle ještě úplně nerozumím
-        for j in indices:
-            field[j] = ch
-    else:
-        print("Špatně. Toto písmeno ve slovu není.")
-        tries += 1
-    return field, tries
-
-
 def didhewinyet(current_field):
     if "_" in current_field:
         return False
     else:
         return True
+
+# def new_game():
+#     w = choose(words)
+#     f = make_it_the_field(w)
+
+
+print("""\nZahrajeme si hru Šibenice.\n
+Budeš hádat, které slovo si myslím.\n
+Pro začátek pár pravidel.
+- Nezapomeň na diakritiku. Písmena "ž" a "z" nejsou to samé!
+- Můžeš psát malá i velká písmena, výsledek bude stejný.
+- Písmeno "ch" zadávej zvlášť jako "c" a "h". \n\nVšechno jasné? Tak hrajeme!\n""")
+
+hra = "ano"
+while hra == "ano":
+    word = choose(words)
+    field = make_it_the_field(word)
+    unsuccessful_tries = 0
+    try:
+        print("""\nPřemýšlím...\n\nUž vím. Můžeš hádat.\nMáš 10 pokusů.\n""")
+        while unsuccessful_tries < 10:
+            field, unsuccessful_tries = turn(field, unsuccessful_tries)
+            pick_picture(unsuccessful_tries)
+            if unsuccessful_tries >= 10:
+                hra = input("Bohužel jsi prohrál. Chceš hrát znovu? Napiš ano/ne.\n").lower()
+                if hra == "ano":
+                    print("\nNOVÁ HRA\n")
+                    break
+                elif hra == "ne":
+                    raise ValueError("End of game")
+            elif didhewinyet(field) == True:
+                    show_current(field)
+                    hra = input("Gratulujeme! Vyhrál jsi. Chceš hrát znovu? Napiš ano/ne.\n").lower()
+                    if hra == "ano":
+                        print("\nNOVÁ HRA\n")
+                        break
+                    elif hra == "ne":
+                        raise ValueError("End of game")
+    except ValueError as e:
+        if e.args[0] == "End of game":
+            print("Dobře. Tak na viděnou příště!")
+        else:
+            raise
